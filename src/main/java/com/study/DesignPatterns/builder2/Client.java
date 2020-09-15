591e11cc8edecc2c1bcf1c7f221270646356541e
Alex Won <dnjscksdn98@gmail.com>
2020-09-15 23:30:06 +0900

    chore: add user dto builder 2

---
+++ src/main/java/com/study/DesignPatterns/builder2/Client.java
@@ -0,0 +1,45 @@
+package com.study.DesignPatterns.builder2;
+
+import com.study.DesignPatterns.builder.Address;
+import com.study.DesignPatterns.builder.User;
+import com.study.DesignPatterns.builder2.UserDto.UserDtoBuilder;
+
+import java.time.LocalDate;
+
+public class Client {
+
+  public static void main(String[] args) {
+    User user = createUser();
+    UserDto dto = directBuild(UserDto.getBuilder(), user);
+    System.out.println(dto);
+  }
+
+  private static UserDto directBuild(UserDtoBuilder builder, User user) {
+    return builder
+            .withFirstName(user.getFirstName())
+            .withLastName(user.getLastName())
+            .withBirth(user.getBirth())
+            .withAddress(user.getAddress())
+            .build();
+  }
+
+  /**
+   *  Description:
+   *      - Returns a sample user
+   *
+   */
+  public static User createUser() {
+    User user = new User();
+    user.setBirth(LocalDate.of(1998, 11, 18));
+    user.setFirstName("Alex");
+    user.setLastName("Won");
+    Address address = new Address();
+    address.setHouseNumber("100");
+    address.setStreet("Non-Hyundong");
+    address.setCity("Apgojung");
+    address.setState("Gangnam");
+    address.setZipCode("12957");
+    user.setAddress(address);
+    return user;
+  }
+}
---
+++ src/main/java/com/study/DesignPatterns/builder2/UserDto.java
@@ -0,0 +1,88 @@
+package com.study.DesignPatterns.builder2;
+
+import com.study.DesignPatterns.builder.Address;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class UserDto {
+
+  private String name;
+  private String address;
+  private String age;
+
+  public String getName() {
+    return name;
+  }
+
+  private void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  private void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  private void setAge(String age) {
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Name: %s\nAge: %s\nAddress: %s", name, age, address);
+  }
+
+  public static UserDtoBuilder getBuilder() {
+    return new UserDtoBuilder();
+  }
+
+  public static class UserDtoBuilder {
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String address;
+    private UserDto userDto;
+
+    public UserDtoBuilder withFirstName(String fname) {
+      firstName = fname;
+      return this;
+    }
+
+    public UserDtoBuilder withLastName(String lname) {
+      lastName = lname;
+      return this;
+    }
+
+    public UserDtoBuilder withBirth(LocalDate birth) {
+      Period ageInYears = Period.between(birth, LocalDate.now());
+      age = Integer.toString(ageInYears.getYears());
+      return this;
+    }
+
+    public UserDtoBuilder withAddress(Address address) {
+      this.address = address.getHouseNumber() + ", " + address.getStreet() + ", " + address.getCity() + ", " + address.getState() + ", " + address.getZipCode();
+      return this;
+    }
+
+    public UserDto build() {
+      this.userDto = new UserDto();
+      userDto.setName(firstName + " " + lastName);
+      userDto.setAddress(address);
+      userDto.setAge(age);
+      return this.userDto;
+    }
+
+    public UserDto getUserDto() {
+      return this.userDto;
+    }
+  }
+}
+
