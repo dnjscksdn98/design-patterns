1ae1f966b01a8df84fcc884671673d004ad1e3ec
Alex Won <dnjscksdn98@gmail.com>
2020-09-18 22:55:54 +0900

    chore: create post models

---
+++ src/main/java/com/study/DesignPatterns/creational/simpleFactory/ProductPost.java
@@ -0,0 +1,28 @@
+package com.study.DesignPatterns.creational.simpleFactory;
+
+/**
+ *  Description:
+ *    - Represents a product information post.
+ *
+ */
+public class ProductPost extends Post {
+
+  private String imageUrl;
+  private String name;
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
